091afc745ab92de367f5bf557ba8c02891d16dda
Jeremy Tinianow <Jerms@Jeremy-Tinianows-MacBook-Pro.local>
2015-03-07 16:27:22 -0700

    Added asnc typekit code and reworked page transitions

--- _site/assets/js/mobile-menu.js
+++ _site/assets/js/mobile-menu.js
@@ -1,11 +1,4 @@
-function changeClass() {
-	if ( document.getElementById("menu-1").className.match(/(?:^|\s)site-title__wrapper(?!\S)/) )
-		document.getElementById("menu-1").className = document.getElementById("menu-1").className.replace ( /(?:^|\s)site-title__wrapper(?!\S)/g , 'site-title__wrapper--visible' );
-	else (
-	document.getElementById("menu-1").className = "site-title__wrapper");
-}
 
-
-window.onload = function(){
-	document.getElementById("nav-primary__menu-toggle").addEventListener( 'click' , changeClass );
-}
+$('.nav-primary__menu-toggle').click(function() {
+	$('.site-title__wrapper').toggleClass('visible');
+});
